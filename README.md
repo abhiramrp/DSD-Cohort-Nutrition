d2a56d9bc485d49d302e65a632c474d2edab2b46
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2024-03-31 02:01:07 -0500

    fetch test

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-run 
+# Full stack app with Nextjs, Express, and PostgreSQL
 
+Run 
+```
 docker-compose up -d
+```
+to set it on your machine. 
+
+
+Frontend - https://dsd-cohort-nutrition.vercel.app/
+
+Backend - https://dsd-cohort-nutrition-backend.vercel.app/
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,15 +5,44 @@ export default function Home() {
   const [message, setMessage] = useState("Loading")
 
   useEffect(() => {
-    fetch("http://localhost:8080/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        console.log(data)
-        setMessage(data.message)
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api");
+        if (!response.ok){
+          throw new Error('Local host error');
+        }
+      
+        const data = await response.json();
+        
+        console.log(data);
+        setMessage(data.message);
+      } catch (error) {
+        console.error("Local Host fetch fail", error);
+        tryVercelFetch();
       }
-    )
+    };
+
+    const tryVercelFetch = async () => {
+      try {
+        const altResponse = await fetch("https://dsd-cohort-nutrition-backend.vercel.app/api");
+        if (!altResponse.ok){
+          throw new Error('Alt host error');
+        }
+      
+        const data = await altResponse.json();
+        
+        console.log(data);
+        setMessage(data.message);
+      } catch (error) {
+        console.error("Vercel Host fetch fail", error);
+      }
+    };
+
+    fetchData();
+
   }, []) 
+
+
 
   return (
     <div>
